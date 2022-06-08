4c27299e6dfc7a1ac6042d846c0aa69d22058dae
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-06-07 23:02:22 -0700

    job roles and other

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 //Use the .focus() method on the <input type="text"> element for the "Name" field.
 const name = document.querySelector("#name")
 name.focus();
+
+const jobRole = document.querySelector("#other-job-role")
+const jobSelect = document.querySelector('#title')
+// console.log(jobRole, jobSelect);
+jobRole.style.display = 'none'
+jobSelect.addEventListener('change', (e)=>{
+
+    if(e.target.value === 'other'){
+        jobRole.style.display = 'block'
+    } else {
+        jobRole.style.display = 'none'
+    }
+})
+
