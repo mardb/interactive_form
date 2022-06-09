f8d407697866e287bb453263431a32191e71e0a2
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-06-08 17:11:12 -0700

    T-shirt info section is finally working!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ name.focus();
 
 const jobRole = document.querySelector("#other-job-role");
 const jobSelect = document.querySelector("#title");
-// console.log(jobRole, jobSelect);
+
 jobRole.style.display = "none";
 jobSelect.addEventListener("change", (e) => {
   console.log(designSelect.value);
@@ -18,32 +18,23 @@ jobSelect.addEventListener("change", (e) => {
 const colorSelect = document.querySelector("#color");
 //selects theme js or puns
 const designSelect = document.querySelector("#design");
-// colorSelect.children
 
-// console.log(colorSelect)
-// console.log(jsPuns)
-// console.log(heartJs);
 colorSelect.setAttribute("disabled", true);
+
 designSelect.addEventListener("change", (e) => {
-  //    console.log(designSelect.value)
   colorSelect.removeAttribute("disabled", false);
+
   for (let i = 0; i < colorSelect.length; i++) {
-    // console.log(options[i]);
-    console.log(colorSelect);
-    // const targetVal = e.target.value;
-    // const dataTheme = options[i].getAttribute("data-theme");
-    //    console.log(targetVal);
-    // console.log(dataTheme);
-    //children of color select
-    if (e.target.value !== colorSelect.children[i].getAttribute("data-theme")) {
-      colorSelect.children[i].disabled = true;
-    //   colorSelect.children[i].setAttribute("selected", false);
-    //   designSelect[i].setAttribute("selected", true);
-    } else{
-        colorSelect.children[i].disabled = false;
-        // colorSelect.children[i].setAttribute("hidden", false);
-  
+    const targetVal = e.target.value;
+    const dataTheme = colorSelect.children[i].getAttribute("data-theme");
+
+    if (targetVal === dataTheme) {
+        colorSelect[i].hidden = false;
+        colorSelect[i].select = true;
     }
-}
- 
+    if (targetVal != dataTheme) {
+        colorSelect[i].hidden = true;
+        colorSelect[i].select = false;
+    }
+  }
 });
