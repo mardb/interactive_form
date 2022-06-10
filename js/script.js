447bff10ec8a4df40a5710e280630ad608712be8
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-06-09 20:28:03 -0700

    refactoring payment info

--- js/script.js
+++ js/script.js
@@ -60,27 +60,19 @@ fieldset.addEventListener("change", (e) => {
   }
 });
 
-//Payment info
-//selects whole select menu
-const payment = document.querySelector("#payment ");
-//options below
-const credit = document.querySelector("#credit-card");
-// console.log(credit.id)
-const paypal = document.querySelector("#paypal");
-// console.log(paypal.id);
-const btc = document.querySelector("#bitcoin");
-// console.log(paypal.id);
+const payments = document.querySelectorAll(".payment-type");
+const paymentType = document.getElementsByTagName("div");
 
-paypal.hidden = true;
-btc.hidden = true;
+payments[1].hidden = true;
+payments[2].hidden = true;
+
 payment.children[1].setAttribute("selected", true);
 
 payment.addEventListener("change", (e) => {
   let eventTarget = e.target.value;
-  let paymentOptions = [credit, paypal, btc];
+  let paymentOptions = payments;
 
   for (let i = 0; i < paymentOptions.length; i++) {
-    //    console.log(paymentOptions[i].id)
     if (eventTarget === paymentOptions[i].id) {
       console.log(eventTarget, paymentOptions[i].id);
       paymentOptions[i].hidden = false;
@@ -88,11 +80,4 @@ payment.addEventListener("change", (e) => {
       paymentOptions[i].hidden = true;
     }
   }
-  // design some conditional logic inside event listener to show or hide content based on what the selection is. Whichever payment option is selected, should be shown, and the other options should be hidden
-  //So it's as simple as turning the display on or off
 });
-
-/*"Payment Info" section
-The credit card payment option should be selected for the user by default. So when the form first loads, "Credit Card" should be displayed in the "I'm going to pay with" <select> element, and the credit card payment section should be the only payment section displayed in the form’s UI. And when the user selects one of the payment options from the "I'm going to pay with" drop down menu, the form should update to display only the chosen payment method section.
-
-Program the "I'm going to pay with" <select> element to listen for user changes. When a change is detected, hide all payment sections in the form’s UI except the selected one.*/
